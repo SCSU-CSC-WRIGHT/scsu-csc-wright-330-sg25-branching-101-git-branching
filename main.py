6d6eed259171467a72e0e19864de694ac438b85b
Hualin Li <hualin120@gmail.com>
2025-02-13 15:29:34 -0500

    Converted code in main.py to a function.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """There are 3 errors in this code. Please use your own ability to find them all. Consider this a Python refresher."""
 def addition():
+    total = 0
     for i in range(5):
     number = input("Enter a number: ")
     total += number

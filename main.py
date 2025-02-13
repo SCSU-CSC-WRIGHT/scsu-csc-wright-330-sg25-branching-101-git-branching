65ec8b73195f443f9d28c85ca06f00c9d3d62892
scrimster <129795353+scrimster@users.noreply.github.com>
2025-02-12 19:06:39 -0500

    Corrected code in main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 """There are 3 errors in this code. Please use your own ability to find them all. Consider this a Python refresher."""
 
+total = 0
+
 for i in range(5):
-    number = input("Enter a number: ")
+    number = int(input("Enter a number: "))
     total += number
 
-print("The running total is: "  total)
+print("The running total is: ", total)

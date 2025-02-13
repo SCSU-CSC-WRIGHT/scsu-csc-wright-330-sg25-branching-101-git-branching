408408e39bf859976186869f598c6ffedbc0df7c
DillonTall <dillontall15@gmail.com>
2025-02-12 19:28:30 -0500

    Corrected code in main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """There are 3 errors in this code. Please use your own ability to find them all. Consider this a Python refresher."""
-total = 0
+total = 0 
 for i in range(5):
     number = int(input("Enter a number: "))
     total += number

9caa43bb40dcbc5472fbf420a7d48fb34c46bdbb
SanteIsHere <r.a.frye00@gmail.com>
2025-02-12 19:03:47 -0500

    Corrected code in 'main.py'

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 """There are 3 errors in this code. Please use your own ability to find them all. Consider this a Python refresher."""
 
+# Fix 1: Initialize variable `total`
+total = 0
+
 for i in range(5):
-    number = input("Enter a number: ")
+    # Fix 2: Convert input into an integer
+    number = int(input("Enter a number: "))
     total += number
 
-print("The running total is: "  total)
+# Fix 3: Add comma to separate arguments to print function
+print("The running total is: ", total)

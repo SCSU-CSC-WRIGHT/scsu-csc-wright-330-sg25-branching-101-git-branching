3f31b5a015ab14ae9c52729228e6662740b4ddbb
scrimster <129795353+scrimster@users.noreply.github.com>
2025-02-15 11:28:19 -0500

    Converted code in main.py to a function.

--- main.py
+++ main.py
@@ -4,14 +4,24 @@
 # Software Design and Development - CSC 330
 # March 2, 20255
 
-# error fix #1: initialize total to 0
-total = 0
 
-for i in range(5):
-    # ask user to input a number
-    # error fix #2: convert user input to an integer
-    number = int(input("Enter a number: "))
-    total += number
+def main():
+    running_total()
 
-# error fix #3: add a comma after the string so the running total appears
-print("The running total is: ",  total)
+
+def running_total():
+    # error fix #1: initialize total to 0
+    total = 0
+
+    for i in range(5):
+        # ask user to input a number
+        # error fix #2: convert user input to an integer
+        number = int(input("Enter a number: "))
+        total += number
+    
+    # error fix #3: add a comma after the string so the running total appears
+    print("The running total is: ",  total)
+
+
+if __name__ == "__main__":
+    main()

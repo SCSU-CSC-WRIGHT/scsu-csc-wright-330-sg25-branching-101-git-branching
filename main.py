66c281e0a6b39c4334be02c33ce6c7295ad930d1
Orlando Marin <orlandomarin@orlandos-air.lan>
2025-02-15 09:15:11 -0500

    Corrected code in main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 """There are 3 errors in this code. Please use your own ability to find them all. Consider this a Python refresher."""
 
+# ORLANDO MARIN
+# Software Design and Development - CSC 330
+# March 2, 20255
+
+# error fix #1: initialize total to 0
+total = 0
+
 for i in range(5):
-    number = input("Enter a number: ")
+    # ask user to input a number
+    # error fix #2: convert user input to an integer
+    number = int(input("Enter a number: "))
     total += number
 
-print("The running total is: "  total)
+# error fix #3: add a comma after the string so the running total appears
+print("The running total is: ",  total)

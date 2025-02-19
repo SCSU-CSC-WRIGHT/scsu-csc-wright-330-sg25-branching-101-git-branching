b3269b8779344d7bf0e4ce10067483177b19221b
harshx1427 <harshx14@gmail.com>
2025-02-19 18:34:39 -0500

    converted main.py to code

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 """There are 3 errors in this code. Please use your own ability to find them all. Consider this a Python refresher."""
-total = 0 
-for i in range(5):
-    number = int(input("Enter a number: "))
-    total += number
+def main():
+    total = 0 
+    for i in range(5):
+        number = int(input("Enter a number: "))
+        total += number
 
-print("The running total is: " + str(total))
+    print("The running total is: " + str(total))
+
+main()

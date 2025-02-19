d884a0042500c83d82991ec3833b5efe7a074550
SanteIsHere <r.a.frye00@gmail.com>
2025-02-19 16:07:05 -0500

    Copy over default main.py to work with Giovanna

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 """There are 3 errors in this code. Please use your own ability to find them all. Consider this a Python refresher."""
-
-# Fix 1: Initialize variable `total`
-total = 0
-
+total = 0 
 for i in range(5):
-    # Fix 2: Convert input into an integer
-    number = int(input("Enter a number: "))
-    total += number
+    number = input("Enter a number: ")
+    total += int(number)
 
-# Fix 3: Add comma to separate arguments to print function
 print("The running total is: ", total)

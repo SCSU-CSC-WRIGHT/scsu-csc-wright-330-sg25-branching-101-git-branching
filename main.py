7af709ecd031450baefb0cef41bb93fe1367d663
Christopher Meeks <meekschris103@gmail.com>
2025-02-20 12:12:03 -0500

    Converted main.py to a function.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 """There are 3 errors in this code. Please use your own ability to find them all. Consider this a Python refresher."""
-total = 0
+total = 0 # Error 1: Added total = 0
 
 for i in range(5):
-    number = int(input("Enter a number: "))
+    number = int(input("Enter a number: ")) # Error 2: Added int
     total += number
 
-print("The running total is:  "+ str(total))
+print("The running total is:  "+ str(total)) # Error 3: Added str()
